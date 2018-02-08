0a0ef8a37334e18ab788669dedf320d7706346d3
Erich Kaderka <erich.kaderka@gmail.com>
2018-02-08 14:50:25 +0100

    Add credo

--- mix.exs
+++ mix.exs
@@ -41,7 +41,8 @@ defmodule Reddex.Mixfile do
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
-      {:distillery, "~> 1.0"}
+      {:distillery, "~> 1.0"},
+      {:credo, "~> 0.9.0-rc1", only: [:dev, :test], runtime: false}
     ]
   end
 
