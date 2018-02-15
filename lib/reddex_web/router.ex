74add890fb1e51b6d03598d61f354fc198d686a9
Erich Kaderka <erich.kaderka@gmail.com>
2018-02-15 14:53:09 +0100

    Add mix format

--- lib/reddex_web/router.ex
+++ lib/reddex_web/router.ex
@@ -7,7 +7,7 @@ defmodule ReddexWeb.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
-    plug PlugSecex
+    plug(PlugSecex)
   end
 
   pipeline :api do
