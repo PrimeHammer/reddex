f958fcb52f7a2641392c9e90e9f23357fdf281c8
Erich Kaderka <erich.kaderka@gmail.com>
2018-03-01 15:27:40 +0100

    Mix format

--- lib/reddex/accounts/accounts.ex
+++ lib/reddex/accounts/accounts.ex
@@ -108,12 +108,16 @@ defmodule Reddex.Accounts do
   @doc "finds an account or creates new one"
   def find_or_create(auth, allowed_emails) do
     cond do
-      check_email_allowed?(auth.info.email, allowed_emails) && (user_from_db = get_user_by_email(auth.info.email)) ->
+      check_email_allowed?(auth.info.email, allowed_emails) &&
+          (user_from_db = get_user_by_email(auth.info.email)) ->
         user_from_db
+
       check_email_allowed?(auth.info.email, allowed_emails) ->
         {:ok, user = %User{}} = create_user(%{name: auth.info.name, email: auth.info.email})
         user
-      true -> {:error, "Your email isn't allowed"}
+
+      true ->
+        {:error, "Your email isn't allowed"}
     end
   end
 
