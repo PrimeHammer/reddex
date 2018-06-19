a3f2e383fcd816ea6736e0bd6a22a34bae424ddd
Erich Kaderka <erich.kaderka@gmail.com>
2018-06-19 16:50:16 +0200

    Remove unneccessary functions from Accounts context

--- test/reddex/accounts/accounts_test.exs
+++ test/reddex/accounts/accounts_test.exs
@@ -29,41 +29,6 @@ defmodule Reddex.AccountsTest do
       assert Accounts.get_user!(user.id) == user
     end
 
-    test "create_user/1 with valid data creates a user" do
-      assert {:ok, %User{} = user} = Accounts.create_user(@valid_attrs)
-      assert user.email == "sefinko@ph.com"
-      assert user.name == "some name"
-    end
-
-    test "create_user/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Accounts.create_user(@invalid_attrs)
-    end
-
-    test "update_user/2 with valid data updates the user" do
-      user = user_fixture()
-      assert {:ok, user} = Accounts.update_user(user, @update_attrs)
-      assert %User{} = user
-      assert user.email == "some updated email"
-      assert user.name == "some updated name"
-    end
-
-    test "update_user/2 with invalid data returns error changeset" do
-      user = user_fixture()
-      assert {:error, %Ecto.Changeset{}} = Accounts.update_user(user, @invalid_attrs)
-      assert user == Accounts.get_user!(user.id)
-    end
-
-    test "delete_user/1 deletes the user" do
-      user = user_fixture()
-      assert {:ok, %User{}} = Accounts.delete_user(user)
-      assert_raise Ecto.NoResultsError, fn -> Accounts.get_user!(user.id) end
-    end
-
-    test "change_user/1 returns a user changeset" do
-      user = user_fixture()
-      assert %Ecto.Changeset{} = Accounts.change_user(user)
-    end
-
     test "get_user_by_email/1 returns the user with given email" do
       user = user_fixture()
       assert user == Accounts.get_user_by_email("sefinko@ph.com")
