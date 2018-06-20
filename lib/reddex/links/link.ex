8802c138fa7b75f336bb72a9f9cf395bd193aa9a
Matej Minarik <mato.minarik@azet.sk>
2018-06-20 14:28:55 +0200

    Add conversion to/from tags_input and array of tags

--- lib/reddex/links/link.ex
+++ lib/reddex/links/link.ex
@@ -7,6 +7,7 @@ defmodule Reddex.Links.Link do
   schema "links" do
     field :description, :string
     field :tags, {:array, :string}
+    field :tags_input, :string, virtual: true
     field :title, :string
     field :url, :string
 
@@ -16,8 +17,22 @@ defmodule Reddex.Links.Link do
   @doc false
   def changeset(link, attrs) do
     link
-    |> cast(attrs, [:url, :tags, :title, :description])
-    |> validate_required([:url, :tags])
+    |> cast(attrs, [:url, :tags_input, :title, :description])
+    |> validate_required([:url, :tags_input])
     |> unique_constraint(:url)
+    |> tags_input_to_tags_array()
+  end
+
+  defp tags_input_to_tags_array(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{tags_input: tags_input}} ->
+        tags = 
+          tags_input
+            |> String.split(",")
+            |> Enum.map(&String.trim/1)
+          put_change(changeset, :tags, tags)
+      _ ->
+        changeset
+    end
   end
 end
