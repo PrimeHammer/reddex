95e7d43450f5ebf4877f4a9c7800fa078d25bec2
Matej Minarik <mato.minarik@azet.sk>
2018-06-23 09:56:12 +0200

    Add Bulma CSS

--- lib/reddex_web/router.ex
+++ lib/reddex_web/router.ex
@@ -7,7 +7,7 @@ defmodule ReddexWeb.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
-    plug(PlugSecex)
+    plug(PlugSecex, except: ["content-security-policy"])
   end
 
   pipeline :api do
