b7849608a82e1ffae1255cf2b79261a1bb35420d
Matej Minarik <mato.minarik@azet.sk>
2018-06-25 09:51:02 +0200

    Fix CSP Headers for FontAwesome

--- lib/reddex_web/router.ex
+++ lib/reddex_web/router.ex
@@ -7,7 +7,9 @@ defmodule ReddexWeb.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
-    plug(PlugSecex, except: ["content-security-policy"])
+    plug(PlugSecex, overrides: [
+      "content-security-policy": "default-src 'self'; script-src 'self' use.fontawesome.com 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline' 'unsafe-eval'"
+    ])
   end
 
   pipeline :api do
