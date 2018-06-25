87da25157b8d778e57a5402e225647e690b8ff5b
Matej Minarik <mato.minarik@azet.sk>
2018-06-25 14:36:36 +0200

    Fix Ebert Comments

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,4 +1,4 @@
 setTimeout(() => {
-  const notifications = document.querySelectorAll(".notification");
+  const notifications = document.querySelectorAll('.notification');
   notifications.forEach(n => n.classList.add('hidden'));
 }, 3500);
