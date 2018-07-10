2f91720a2695101dcfb8ddcb4c4ba35fa0078207
Matej Minarik <mato.minarik@azet.sk>
2018-07-10 13:47:06 +0200

    Add RxJS

--- assets/brunch-config.js
+++ assets/brunch-config.js
@@ -2,8 +2,10 @@ exports.config = {
   // See http://brunch.io/#documentation for docs.
   files: {
     javascripts: {
-      joinTo: "js/app.js"
-
+      joinTo: {
+        'js/app.js': /^js/,
+        'js/vendor.js': /^node_modules/
+      }
       // To use a separate vendor.js bundle, specify two files path
       // http://brunch.io/docs/config#-files-
       // joinTo: {
